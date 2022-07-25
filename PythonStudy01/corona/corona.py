42fc8af145b12cd7c8c87e09f8c45fb48d419d19
hondatoshihiro <harutokaho@gmail.com>
2022-07-26 08:11:40 +0900

    1週間平均に変更

--- PythonStudy01/corona/corona.py
+++ PythonStudy01/corona/corona.py
@@ -18,13 +18,39 @@ def main():
     cnt = 0
     prenum = 0
     datelist.reverse()
+    alist = []
     for datedata in datelist:
+        blist = []
         if datedata["name_jp"] == "福岡県":
             if cnt == 0:
                 prenum = int(datedata["npatients"])
             print(datedata["date"] + ":" + str(int(datedata["npatients"])-prenum))
             gdatelist.append(datedata["date"])
-            gpatientsnumlist.append(int(datedata["npatients"])-prenum)
+            #1週間平均感染者数算出 start
+            alist.append( int(datedata["npatients"])-prenum )
+            index = 0
+            sum = 0
+            print("テスト1 len(alist):" + str(len(alist)))
+            for a in alist:
+                sum = sum + a
+                index = index + 1
+            gpatientsnumlist.append( sum/len(alist))
+            if len(alist) == 7:
+                print("テスト2 len(alist):" + str(len(alist)))
+                for index in range(len(alist)):
+                    if index != 0:
+                        blist.append(alist[index])
+                    #print("テスト index:" + str(index))
+            else:
+                blist = alist
+            alist.clear
+            alist = blist
+            #print("テスト3 len(alist):" + str(len(alist)))
+            #print("テスト3 len(blist):" + str(len(blist)))
+            #1週間平均感染者数算出 end
+            #1日の感染者数 start
+            #gpatientsnumlist.append(int(datedata["npatients"])-prenum)
+            #1日の感染者数 end
             prenum = int(datedata["npatients"])
             cnt = cnt+1
     plt.xlabel("date", fontsize=12)
