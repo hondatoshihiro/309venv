3ca5d52732b6591c0939d9f5846fa52dbbb0f366
hondatoshihiro <harutokaho@gmail.com>
2022-08-20 18:17:26 +0900

    ログ集計

--- PythonStudy01/HondaWorks/CollectSYCLog.py
+++ PythonStudy01/HondaWorks/CollectSYCLog.py
@@ -1,10 +1,16 @@
+import os
 
 def main():
-    CollectLog()
+    CollectLog("d:\\work\\result")
 
-def CollectLog():
-    with open("result_log.txt", "r", encoding="utf_8") as target_file:
-        pass
+def CollectLog(targetPath):
+    print(targetPath)
+    #targetPath探索
+    targetList=os.listdir(targetPath)
+    print(len(targetList))
+    for targetFile in targetList:
+        with open(targetFile, "r", encoding="utf_8") as tf:
+            for line in tf:
 
 if __name__ == '__main__':
     main()
